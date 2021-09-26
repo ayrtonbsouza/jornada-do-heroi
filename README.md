04f32a7b174aa810c0aaaf5a3666a207b561c07c
Ayrton Souza <ayrtonsouza@icloud.com>
2021-09-25 23:38:24 -0300

    [:tada:] challenge complete

---
+++ README.md
@@ -0,0 +1,6 @@
+# Nome dos Integrantes do Grupo:
+- Ayrton Barbosa de Souza - 21417305
+- Bianca Gualter Pereira - 21398306
+- Danilo Kenji Banno - 21434446
+- Larissa Leonor V Costa - 21404256
+- Gabriel Pêgo Lima - 21454179
--- index.py
+++ index.py
@@ -1,72 +1,91 @@
 from random import randint, randrange
 
+rodada = 0
+
 
 def sala_1():
     opcao = input(
         'Sala 1: \nSelecione um caminho: \n 1 - Direita \n 2 - Esquerda\n')
-    if opcao == '1':
+    if opcao == '1' and rodada < 7:
         sala_2()
-    elif opcao == '2':
+    elif opcao == '2' and rodada < 7:
         sala_3()
+    else:
+        fim_de_jogo()
 
 
 def sala_2():
     opcao = input(
         'Sala 2: \nSelecione um caminho: \n 1 - Direita \n 2 - Esquerda\n')
-    if opcao == '1':
+    if opcao == '1' and rodada < 7:
         sala_3()
-    elif opcao == '2':
+    elif opcao == '2' and rodada < 7:
         sala_4()
+    else:
+        fim_de_jogo()
 
 
 def sala_3():
     opcao = input(
         'Sala 3: \nSelecione um caminho: \n 1 - Direita \n 2 - Esquerda\n')
-    if opcao == '1':
+    if opcao == '1' and rodada < 7:
         sala_4()
-    elif opcao == '2':
+    elif opcao == '2' and rodada < 7:
         sala_5()
+    else:
+        fim_de_jogo()
 
 
 def sala_4():
     opcao = input(
         'Sala 4: \nSelecione um caminho: \n 1 - Direita \n 2 - Esquerda\n')
-    if opcao == '1':
+    if opcao == '1' and rodada < 7:
         sala_5()
-    elif opcao == '2':
+    elif opcao == '2' and rodada < 7:
         sala_6()
+    else:
+        fim_de_jogo()
 
 
 def sala_5():
     opcao = input(
         'Sala 5: \nSelecione um caminho: \n 1 - Direita \n 2 - Esquerda\n')
-    if opcao == '1':
+    if opcao == '1' and rodada < 7:
         sala_6()
-    elif opcao == '2':
+    elif opcao == '2' and rodada < 7:
         sala_7()
+    else:
+        fim_de_jogo()
 
 
 def sala_6():
     print('Sala 6: \nAqui você só tem um caminho, o da Esquerda\n, seguindo para próxima sala.\n')
-    sala_8()
+    if rodada < 7:
+        sala_8()
+    else:
+        fim_de_jogo()
 
 
 def sala_7():
     opcao = input(
         'Sala 7: \nSelecione um caminho: \n 1 - Direita \n 2 - Esquerda\n')
-    if opcao == '1':
+    if opcao == '1' and rodada < 7:
         sala_8()
-    elif opcao == '2':
+    elif opcao == '2' and rodada < 7:
         sala_9()
+    else:
+        fim_de_jogo()
 
 
 def sala_8():
     opcao = input(
         'Sala 8: \nSelecione um caminho: \n 1 - Direita \n 2 - Esquerda\n')
-    if opcao == '1':
+    if opcao == '1' and rodada < 7:
         sala_9()
-    elif opcao == '2':
+    elif opcao == '2' and rodada < 7:
         portal()
+    else:
+        fim_de_jogo()
 
 
 def sala_9():
@@ -86,6 +105,10 @@ def portal():
         sala_4()
     elif randomico == '5':
         sala_5()
+
+
+def fim_de_jogo():
+    print('Game Over:\nVocê atingiu 7 jogadas antes de chegar ao final.')
 
 
 sala_1()
